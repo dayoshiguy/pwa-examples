28f5ac56d880980ef64d2d526e0468ad929bbbea
LukeTheDank <lukecardozaisawesome@gmail.com>
2020-02-04 10:32:38 -0500

    Update sw.js

--- a2hs/sw.js
+++ a2hs/sw.js
@@ -1,7 +1,7 @@
 self.addEventListener('install', function(e) {
  e.waitUntil(
    caches.open('video-store').then(function(cache) {
-     return cache.addAll([
+     return cache.addAll([ //put all of the files that you want to cache here. if ur not sure just put all of them
        '/pwa-examples/a2hs/',
        '/pwa-examples/a2hs/index.html',
        '/pwa-examples/a2hs/index.js',
